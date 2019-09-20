465e1400ad61eb263d26afffbf9559f42ffe1b1a
VISTAPRINTUS\jquint <jelle@albumprinter.com>
2019-09-20 11:52:46 +0200

    Move Vsphere windows server module

---
+++ modules/vsphere-windows-server/server.tf
@@ -0,0 +1,58 @@
+locals {
+  servername = "${var.application_environment}${var.application_domain}"
+}
+
+resource "vsphere_virtual_machine" "vsphereserver_on_datastore1" {
+  count = "${var.amount_of_servers}"
+  name = "${local.servername}${count.index + 1}"
+  resource_pool_id = "${var.vsphere_resource_pool_id}"
+  datastore_id     = "${count.index%2==0 ?var.vsphere_datastore_datastore1:var.vsphere_datastore_datastore2}"
+  num_cpus = "${var.vm_cpu}"
+  memory = "${var.vm_memory}"
+  guest_id = "${var.vm_windows_version}"
+  scsi_type = "${var.scsi_type}"
+  wait_for_guest_net_timeout = 0
+  network_interface {
+    network_id = "${var.vm_network_name}"
+  }
+
+  clone {
+    template_uuid = "${var.vm_template_uuid}"
+
+    customize {
+      windows_options {
+        computer_name = "${local.servername}${count.index + 1}"
+        join_domain = "${var.vm_domain}"
+        domain_admin_user = "${var.vm_domain_admin_user}"
+        domain_admin_password = "${var.vm_domain_admin_password}"
+        admin_password = "Test123!"
+        full_name = "Administrator"
+        organization_name = "Albelli BV"
+        auto_logon = "true"
+        auto_logon_count = "2"
+        time_zone = "110"
+      }
+      network_interface {
+        ipv4_address = "${var.vm_ipv4_address}.${var.vm_ipv4_address_host + count.index}"
+        ipv4_netmask = "${var.vm_ipv4_subnet_mask}"
+        dns_server_list = "${var.vm_dns_server_list}"
+      }
+      ipv4_gateway = "${var.vm_ipv4_gateway}"
+
+    }
+
+
+  }
+    disk {
+      label = "disk0"
+      size = "${var.vm_disk_1_size}"
+      thin_provisioned = "${var.vm_thin_provisioned_disk_1}"
+
+    }
+    disk {
+      label = "disk1"
+      unit_number = 1
+      size = "${var.vm_disk2_size}"
+      thin_provisioned = false
+  }
+}
---
+++ modules/vsphere-windows-server/variables.tf
@@ -0,0 +1,48 @@
+variable "aws_region" {
+ default = "eu-west-1"
+}
+variable "aws_shared_services_account_id" {
+  default = "973160909116"
+}
+variable "vsphere_vcenter_name" {}
+variable "vsphere_datacenter" {}
+variable "vsphere_compute_cluster" {}
+variable "vsphere_resource_pool_id" {}
+variable "vsphere_datastore_datastore1" {}
+variable "vsphere_datastore_datastore2" {}
+variable "application_environment" {}
+variable "application_domain" {}
+variable "amount_of_servers" {
+  default = 2
+}
+variable "vm_template_uuid" {}
+variable "vm_windows_version" {
+  default = "windows8Server64Guest"
+}
+variable "vm_domain" {}
+variable "vm_domain_admin_user" {}
+variable "vm_domain_admin_password" {}
+variable "vm_workgroup" {}
+variable "vm_cpu" {
+  default = 2
+}
+variable "vm_memory" {
+  default = 4096
+}
+variable "vm_disk_1_size" {}
+variable "vm_thin_provisioned_disk_1" {}
+variable "vm_disk2_size" {
+  default = 20
+}
+variable "vm_network_name" {}
+variable "vm_ipv4_address" {}
+variable "vm_ipv4_address_host" {}
+variable "vm_ipv4_subnet_mask" {}
+variable "vm_ipv4_gateway" {}
+variable "vm_dns_server_list" {type="list"}
+//variable "vm_dnsserver1" {}
+//variable "vm_dnsserver2" {}
+variable "scsi_type" {
+  default = "lsilogic-sas"
+}
+
