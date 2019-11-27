64f758266919c54b16650aa4e2af9a9c67743bf9
Fernando Gonçalves <fernandosg88@gmail.com>
2019-11-27 17:47:03 +0100

    Fixes bug when deciding to use Lambda boilerplate (#16)

--- modules/aws/lambda_function/locals.tf
+++ modules/aws/lambda_function/locals.tf
@@ -2,8 +2,8 @@ locals {
   function_dead_letter_target_name = var.function_dead_letter_target_name != null ? var.function_dead_letter_target_name : "${var.function_name}_dead_letter"
 
   function_handler   = var.function_handler != null ? var.function_handler : lookup(local.handlers, local.runtime, null)
-  function_s3_bucket = var.function_s3_bucket != null ? var.function_s3_bucket : "infra-terraform-modules-lambda-boilerplate"
-  function_s3_key    = var.function_s3_key != null ? var.function_s3_key : "${replace(var.function_runtime, "/[0-9]+.*/", "")}/boilerplate.zip"
+  function_s3_bucket = var.function_s3_bucket == null && var.function_filename == null ? "infra-terraform-modules-lambda-boilerplate" : var.function_s3_bucket
+  function_s3_key    = var.function_s3_key == null && var.function_filename == null ? "${replace(var.function_runtime, "/[0-9]+.*/", "")}/boilerplate.zip" : var.function_s3_key
 
   runtime = replace(var.function_runtime, "/[0-9]+.*/", "")
 
