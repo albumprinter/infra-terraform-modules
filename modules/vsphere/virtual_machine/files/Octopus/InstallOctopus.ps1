a233d615db27fdf395061d7ed47a6b7b4a7a8467
VISTAPRINTUS\jquint <jelle@albumprinter.com>
2020-01-09 13:51:50 +0100

    Adds vsphere module

---
+++ modules/vsphere/virtual_machine/files/Octopus/InstallOctopus.ps1
@@ -0,0 +1,10 @@
+
+Set-Location "C:\Program Files\Octopus Deploy\Tentacle"
+
+.\Tentacle.exe create-instance --instance "Tentacle" --config "D:\Octopus\Tentacle.config" --console
+.\Tentacle.exe new-certificate --instance "Tentacle" --if-blank --console
+.\Tentacle.exe configure --instance "Tentacle" --reset-trust --console
+.\Tentacle.exe configure --instance "Tentacle" --home "D:\Octopus" --app "D:\Octopus\Applications" --port "10933" --console
+.\Tentacle.exe configure --instance "Tentacle" --trust "${octopus_trust}" --console
+.\Tentacle.exe register-with --instance "Tentacle" --server "https://octopus.deploy.photoconnector.net" --apiKey="${octopus_apikey}" --role "${octopus_role1}" --role "${octopus_role2}" --role "${octopus_role3}" --environment "${octopus_environment}" --comms-style TentaclePassive --console
+.\Tentacle.exe service --instance "Tentacle" --install --start --console
