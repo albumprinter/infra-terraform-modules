25ea4b1cb37dcb90721cd33b00ae4920bfe9d7e6
Fernando Goncalves <fernando.goncalves@albelli.com>
2020-04-09 11:12:59 +0200

    Adds check to output value of conditional resources

--- modules/aws/ecr_repository/outputs.tf
+++ modules/aws/ecr_repository/outputs.tf
@@ -1,11 +1,11 @@
 output "ecr_repository" {
-  value = var.provision == true ? aws_ecr_repository.repo[0] : null
+  value = var.provision == true && length(aws_ecr_repository.repo) > 0 ? aws_ecr_repository.repo[0] : null
 }
 
 output "ecr_repository_policy" {
-  value = var.provision == true && var.aws_organization_id != null ? aws_ecr_repository_policy.policy[0] : null
+  value = var.provision == true && var.aws_organization_id != null && length(aws_ecr_repository_policy.policy) > 0 ? aws_ecr_repository_policy.policy[0] : null
 }
 
 output "ecr_lifecycle_policy" {
-  value = var.provision == true && var.max_number_of_images != null ? aws_ecr_lifecycle_policy.lifecycle_policy[0] : null
+  value = var.provision == true && var.max_number_of_images != null && length(aws_ecr_lifecycle_policy.lifecycle_policy) > 0 ? aws_ecr_lifecycle_policy.lifecycle_policy[0] : null
 }
