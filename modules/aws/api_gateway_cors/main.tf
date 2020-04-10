a5b8ae83eafd66f2fa5a8cc2f0bf94fd6dd13ad4
Fernando Goncalves <fernando.goncalves@albelli.com>
2020-04-10 15:21:58 +0200

    Update CORS response_parameters

--- modules/aws/api_gateway_cors/main.tf
+++ modules/aws/api_gateway_cors/main.tf
@@ -30,9 +30,9 @@ resource "aws_api_gateway_method_response" "http_400" {
     "application/json" = "Error"
   }
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = false,
-    "method.response.header.Access-Control-Allow-Methods" = false,
-    "method.response.header.Access-Control-Allow-Origin"  = false
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
   depends_on = [aws_api_gateway_method.this]
 }
@@ -46,9 +46,9 @@ resource "aws_api_gateway_method_response" "http_500" {
     "application/json" = "Error"
   }
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = false,
-    "method.response.header.Access-Control-Allow-Methods" = false,
-    "method.response.header.Access-Control-Allow-Origin"  = false
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
   depends_on = [aws_api_gateway_method.this]
 }
