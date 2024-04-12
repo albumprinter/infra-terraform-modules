f2cb62d1433dd35c17ada103518dba19e3b1b96e
Dan Murray <Dan.Murray@LDN-JCRGP4C7QL.lan>
2024-04-12 16:33:10 +0100

    chore: SRE-3124 - updated ecr_repository base module

--- examples/aws/1.5/ecr_repository/main.tf
+++ examples/aws/1.5/ecr_repository/main.tf
@@ -1,6 +1,6 @@
 module "ecr_repository" {
   source = "../../../../modules/aws/ecr_repository"
-  name                  = "${var.project_name}EcrRepository"
+  name                  = lower("${var.project_name}EcrRepository")
   organization_id                  = "${var.organization_id}"
   tags = var.tags
 }
