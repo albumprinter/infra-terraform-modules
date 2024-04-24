1b47c9ff5241ddca528bdc7978615c96f678a10e
Daniel Betancur <daniel.betancur@albelli.com>
2024-04-24 13:51:42 +0200

    Updating aws provider

--- examples/aws/1.5/examples.tf
+++ examples/aws/1.5/examples.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.5.2"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.46.0"
     }
   }
 }
--- modules/vsphere/virtual_machine/versions.tf
+++
@@ -1,5 +0,0 @@
-terraform {
-  required_providers {
-    aws    = ">= 3.53.0"
-  }
-}
