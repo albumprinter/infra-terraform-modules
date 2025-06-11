5677b1ff9ff771442f4fb5575f7e33cd84b43ee9
Mykola Borka <mykola.borka.extern@posterxxl.com>
2025-06-11 11:46:28 +0300

    add reserved_concurrent_executions variable with default value of "no limit"

--- modules/aws/lambda_function/variables.tf
+++ modules/aws/lambda_function/variables.tf
@@ -73,7 +73,10 @@ variable "error_ok_actions" {
   description = "Actions that should be performed in case the error alarm is in the ok state"
   default     = []
 }
-
+variable "reserved_concurrent_executions" {
+  description = "Reserved concurrent executions for the Lambda function"
+  default     = -1
+}
 variable "publish" {
   description = "Whether to publish creation/change as new Lambda Function Version."
   default     = false
